71c91a27c075a37aa7dfcb801112a96d29bdd52f
LaurenzOM <l.ohnemueller@student.maastrichtuniversity.nl>
2020-03-25 20:41:30 +0100

    area percepts

--- src/Agents/AgentFactory.java
+++ src/Agents/AgentFactory.java
@@ -7,12 +7,20 @@ import java.util.List;
 
 public class AgentFactory {
 
-    static public List<Guard> createGuards(int number) {
+
+    static public List<Guard> createGuards(int number, String guardname) {
 
         List<Guard> guards = new ArrayList<Guard>();
-        for(int i = 0; i < number; i++) {
-            Agents.Guard guard =  new Agents.Guard();
-            guards.add(guard);
+        if (guardname.equals("Guard")) {
+            for (int i = 0; i < number; i++) {
+                Agents.Guard guard = new Agents.Guard();
+                guards.add(guard);
+            }
+        } else {
+            for (int i = 0; i < number; i++) {
+                Agents.RandomGuard guard = new Agents.RandomGuard();
+                guards.add(guard);
+            }
         }
         return guards;
     }
